9d2b8b6ece723424059a7cd1ff436a6357bd24cf
TalitAlves <88683074+TalitAlves@users.noreply.github.com>
2021-09-28 16:36:26 +0200

    Add changeUnit function

--- src/index.js
+++ src/index.js
@@ -44,31 +44,11 @@ if (minutes < 10) {
   actualHour.innerHTML = `${day} - ${hour}:${minutes}`;
 }
 
-//print city form
-/*function actualCity(event) {
-  event.preventDefault();
-  let userTypeCity = document.querySelector("#type-city");
-  let city = document.querySelector("#city");
-  city.innerHTML = userTypeCity.value;
-}
+window.load = getCurrentPosition();
 
-let formCity = document.querySelector("#searching");
-formCity.addEventListener("submit", actualCity);
-
-*/
-
-//week 5 Homework
-
-/*function getCityName(event) {
-  event.preventDefault();
-  let userTypeCity = document.querySelector("#type-city");
-  let cityValue = userTypeCity.value;
-
-  return cityValue;
-}
-
-*/
+let unit = "C";
 let apiKey = "0e49051742904728363d076005947b7c";
+
 let button = document.querySelector("#current-location");
 button.addEventListener("click", getCurrentPosition);
 
@@ -95,18 +75,16 @@ function showCurrentLocationData(position) {
 }
 
 //change the main box data
+
 function updateDataMainForecast(response) {
   console.log(response);
-  let temperature = Math.round(response.data.main.temp);
+  temperature = Math.round(response.data.main.temp);
   let location = response.data.name;
   let currentTemperature = document.querySelector("#temperature");
   currentTemperature.innerHTML = `${temperature}ºC`;
 
   let mainForecast = document.querySelector("#main-forecast");
   mainForecast.innerHTML = response.data.weather[0].main;
-
-  let windSpeed = document.querySelector("#wind-speed");
-  windSpeed.innerHTML = `Wind speed: ${Math.round(response.data.wind.speed)} `;
 
   let minMainForecast = document.querySelector("#min-main-forecast");
   minMainForecast.innerHTML = Math.round(response.data.main.temp_min) + "ºC";
@@ -123,86 +101,84 @@ function updateDataMainForecast(response) {
   icon.src = `http://openweathermap.org/img/wn/${linkIcon}@2x.png`;
 }
 
+let temperature;
+
 function updateForecast5Days(response) {
   console.log(response);
   //Day 1
-  let day0 = document.querySelector("#day0");
-  day0.innerHTML = getDate(response.data.daily[1].dt);
-
-  let day0Forecast = document.querySelector("#day0-forecast");
-  day0Forecast.innerHTML = capitalizeStringForecast5Days(
-    response.data.daily[1].weather[0].description
-  );
-
-  let minDay0 = document.querySelector("#min-day0");
-  minDay0.innerHTML = Math.round(response.data.daily[1].temp.min) + "ºC";
-
-  let maxDay0 = document.querySelector("#max-day0");
-  maxDay0.innerHTML = Math.round(response.data.daily[1].temp.max) + "ºC";
-
-  let linkIcon = response.data.daily[1].weather[0].icon;
-  let icon = document.querySelector("#icon-day1-forecast");
-  icon.src = `http://openweathermap.org/img/wn/${linkIcon}@2x.png`;
-
-  //day 2
   let day1 = document.querySelector("#day1");
-  day1.innerHTML = getDate(response.data.daily[2].dt);
+  day1.innerHTML = getDate(response.data.daily[1].dt);
 
   let day1Forecast = document.querySelector("#day1-forecast");
   day1Forecast.innerHTML = capitalizeStringForecast5Days(
-    response.data.daily[2].weather[0].description
+    response.data.daily[1].weather[0].description
   );
 
   let minDay1 = document.querySelector("#min-day1");
-  minDay1.innerHTML = Math.round(response.data.daily[2].temp.min) + "ºC";
+  minDay1.innerHTML = Math.round(response.data.daily[1].temp.min) + "ºC";
 
   let maxDay1 = document.querySelector("#max-day1");
-  maxDay1.innerHTML = Math.round(response.data.daily[2].temp.max) + "ºC";
+  maxDay1.innerHTML = Math.round(response.data.daily[1].temp.max) + "ºC";
 
-  //day 3
+  //day 2
   let day2 = document.querySelector("#day2");
-  day2.innerHTML = getDate(response.data.daily[3].dt);
+  day2.innerHTML = getDate(response.data.daily[2].dt);
 
   let day2Forecast = document.querySelector("#day2-forecast");
   day2Forecast.innerHTML = capitalizeStringForecast5Days(
-    response.data.daily[3].weather[0].description
+    response.data.daily[2].weather[0].description
   );
 
   let minDay2 = document.querySelector("#min-day2");
-  minDay2.innerHTML = Math.round(response.data.daily[3].temp.min) + "ºC";
+  minDay1.innerHTML = Math.round(response.data.daily[2].temp.min) + "ºC";
 
   let maxDay2 = document.querySelector("#max-day2");
-  maxDay2.innerHTML = Math.round(response.data.daily[3].temp.max) + "ºC";
+  maxDay1.innerHTML = Math.round(response.data.daily[2].temp.max) + "ºC";
 
-  //day 4
+  //day 3
   let day3 = document.querySelector("#day3");
-  day3.innerHTML = getDate(response.data.daily[4].dt);
+  day3.innerHTML = getDate(response.data.daily[3].dt);
 
   let day3Forecast = document.querySelector("#day3-forecast");
   day3Forecast.innerHTML = capitalizeStringForecast5Days(
-    response.data.daily[4].weather[0].description
+    response.data.daily[3].weather[0].description
   );
 
   let minDay3 = document.querySelector("#min-day3");
-  minDay3.innerHTML = Math.round(response.data.daily[4].temp.min) + "ºC";
+  minDay3.innerHTML = Math.round(response.data.daily[3].temp.min) + "ºC";
 
   let maxDay3 = document.querySelector("#max-day3");
-  maxDay3.innerHTML = Math.round(response.data.daily[4].temp.max) + "ºC";
+  maxDay3.innerHTML = Math.round(response.data.daily[3].temp.max) + "ºC";
 
-  //day 5
+  //day 4
   let day4 = document.querySelector("#day4");
-  day4.innerHTML = getDate(response.data.daily[5].dt);
+  day4.innerHTML = getDate(response.data.daily[4].dt);
 
-  let day4Forecast = document.querySelector("#day4-forecast");
+  let day4Forecast = document.querySelector("#day4Forecast");
   day4Forecast.innerHTML = capitalizeStringForecast5Days(
-    response.data.daily[5].weather[0].description
+    response.data.daily[4].weather[0].description
   );
 
   let minDay4 = document.querySelector("#min-day4");
-  minDay4.innerHTML = Math.round(response.data.daily[5].temp.min) + "ºC";
+  minDay4.innerHTML = Math.round(response.data.daily[4].temp.min) + "ºC";
 
   let maxDay4 = document.querySelector("#max-day4");
-  maxDay4.innerHTML = Math.round(response.data.daily[5].temp.max) + "ºC";
+  maxDay4.innerHTML = Math.round(response.data.daily[4].temp.max) + "ºC";
+
+  //day 5
+  let day5 = document.querySelector("#day5");
+  day5.innerHTML = getDate(response.data.daily[5].dt);
+
+  let day5Forecast = document.querySelector("#day5-forecast");
+  day5Forecast.innerHTML = capitalizeStringForecast5Days(
+    response.data.daily[5].weather[0].description
+  );
+
+  let minDay5 = document.querySelector("#min-day5");
+  minDay5.innerHTML = Math.round(response.data.daily[5].temp.min) + "ºC";
+
+  let maxDay5 = document.querySelector("#max-day5");
+  maxDay5.innerHTML = Math.round(response.data.daily[5].temp.max) + "ºC";
 }
 
 function showPosisitionForecast5Days(position) {
@@ -250,7 +226,6 @@ function getCurrentPosition() {
   );
 }
 
-
 function getCurrentForecastByCityName(userCityName) {
   return `https://api.openweathermap.org/data/2.5/weather?q=${userCityName}&appid=${apiKey}&units=metric`;
 }
@@ -260,19 +235,31 @@ function getCurrentForecastByPosition(latitude, longitude) {
 }
 
 function getForecast5DaysByPosition(latitude, longitude) {
-  return `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric´;
+  return `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 }
 
-function changeToCelsius() {
-  let temperatureCelsius = document.querySelector("#temperature");
-  temperatureCelsius.innerHTML = "28℃";
-}
-let celsius = document.querySelector("#celsius");
-celsius.addEventListener("click", changeToCelsius);
-
-function changeToFahrenheit() {
-  let temperatureFahrenheit = document.querySelector("#temperature");
-  temperatureFahrenheit.innerHTML = "82℉";
-}
 let fahrenheit = document.querySelector("#fahrenheit");
-fahrenheit.addEventListener("click", changeToFahrenheit);
+fahrenheit.addEventListener("click", changeUnitToFahrenheit);
+
+let celsius = document.querySelector("#celsius");
+celsius.addEventListener("click", changeUnitToCelsius);
+
+function changeUnitToFahrenheit(event) {
+  event.preventDefault();
+  if (unit !== "F") {
+    let temperatureElement = document.querySelector("#temperature");
+    let toFahrenheit = (temperature * 9) / 5 + 32;
+
+    temperatureElement.innerHTML = `${Math.round(toFahrenheit)} ºF`;
+    return (unit = "F");
+  }
+}
+
+function changeUnitToCelsius(event) {
+  event.preventDefault();
+  if (unit !== "C") {
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = `${temperature}ºC`;
+    return (unit = "C");
+  }
+}
